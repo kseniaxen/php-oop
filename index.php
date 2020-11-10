93fe5d4f0967c977e0f64163d103aca8ce6bcd7d
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2020-11-10 17:05:46 +0200

    Add constructor

--- index.php
+++ index.php
@@ -11,18 +11,32 @@ class MathComplex {
         $this->re += $mc->re;
         $this->im += $mc->im;
     } */
+
+    public function __construct(...$args)
+    {
+        if (count($args) === 1 && $args[0] instanceof MathComplex) {
+            $this->re = $args[0]->re;
+            $this->im = $args[0]->im;
+        }else{
+            $this->re = $args[0];
+            $this->im = $args[1];
+        }
+    }
+
     function add(...$args): void {
         // var_dump($args);
         if (count($args) === 1 && $args[0] instanceof MathComplex) {
             $this->re += $args[0]->re;
             $this->im += $args[0]->im;
-        } else if(floatval($args[0]) !== false && floatval($args[1]) !== false) {
+        }
+        /* else if(floatval($args[0]) !== false && floatval($args[1]) !== false) {
             $this->re += $args[0];
             $this->im += $args[1];
-        }
+        }*/
         /*$this->re += $re;
         $this->im += $im;*/
     }
+
     public function __toString()
     {
         return "re = $this->re; im = $this->im";
@@ -40,6 +54,11 @@ $mc2->im = 1;
 // $mc2->add(1, 1);
 $mc1->add($mc2);
 
-// $mc1->add(1, 1);
-// var_dump($mc1);
-echo $mc1;
+$mc3 = new MathComplex(10, 2);
+echo $mc3 . "\n";
+$mc4 = new MathComplex();
+$mc4->re = 10;
+$mc4->im = 18;
+
+$mc5 = new MathComplex($mc4);
+echo $mc5;
